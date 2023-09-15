b76357fb7e233cd0baaa8c2d33b74d264119ca92
Dev-steadyBoy <85480086+Dev-steadyBoy@users.noreply.github.com>
2023-09-15 16:35:28 +0900

    ADD - JS Scroll (fade event, focus event)

--- src/main.js
+++ src/main.js
@@ -18,3 +18,57 @@ document.addEventListener('scroll', () => {
 document.addEventListener('scroll', () => {
   header.style.opacity = 1 - window.scrollY / headerHeight;
 });
+
+// scrolling으로 인한 바디 컨텐츠의 fade-in효과와 index focus 기능
+const sectionClass = [
+  '.about',
+  '.skills',
+  '.projects',
+  '.study',
+  '.share',
+  '.other',
+];
+const navItems = sectionClass.map((el) => document.querySelectorAll(`${el}`));
+let activeNavItem = navItems[0];
+const targets = document.querySelectorAll('.fade-class');
+const visibleSections = sectionClass.map(() => false);
+const observerCallback = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      const container = entry.target;
+      const index = sectionClass.indexOf(`.${container.id}`);
+
+      visibleSections[index] = entry.isIntersecting;
+      const lastIndex = selectLastOne(visibleSections);
+
+      if (entry.isIntersecting) {
+        container.classList.add('fade-in');
+      } else {
+        container.classList.remove('fade-in');
+      }
+
+      selectNavItem(lastIndex);
+    });
+  },
+  { threshold: 0.35 }
+);
+
+function selectNavItem(index) {
+  const navItem = navItems[index];
+  activeNavItem.forEach((item) => {
+    item.classList.remove('navIdx-focus');
+  });
+  activeNavItem = navItem;
+  activeNavItem.forEach((item) => {
+    item.classList.add('navIdx-focus');
+  });
+}
+
+function selectLastOne(arr) {
+  const idx = arr.lastIndexOf(true);
+  return idx >= 0 ? idx : 0;
+}
+
+targets.forEach((target) => {
+  observerCallback.observe(target);
+});
